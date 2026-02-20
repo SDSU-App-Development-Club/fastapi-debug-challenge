8aefe225660a532258dc1426ef7d120f3fc548b6
jadengong <ong.jadengabriel@gmail.com>
2026-02-19 17:00:38 -0800

    Create main.py for bug #2

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+users_db = {
+    1: {"name": "Alice"},
+    2: {"name": "Bob"}
+}
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    if user_id not in users_db:
+        raise HTTPException(status_code=500, detail="User not found")
+    return users_db[user_id]
