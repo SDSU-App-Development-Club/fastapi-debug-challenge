130475c162c23aabcfcbd9352c09257371bceb0c
jadengong <ong.jadengabriel@gmail.com>
2026-02-19 17:01:52 -0800

    Create main.py for bug #4

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_available: bool
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"item": item, "message": "Item created"}
