97ce5e5417877c8c672dba542a5285c6e60fa1e0
Jaden Ong <ong.jadengabriel@gmail.com>
2026-02-20 12:09:34 -0800

    update bug1

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get("/hello")
+def say_hello():
     return {"message": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.gett("/goodbye")
+def say_goodbye():
+    return {"message": "Goodbye"}
