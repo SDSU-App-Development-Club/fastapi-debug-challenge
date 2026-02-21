65307e3ec444a0265f9ff582b43a10157fa11326
jadengong <ong.jadengabriel@gmail.com>
2026-02-20 17:07:10 -0800

    update bonus 3

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def read_profile(user: dict = Depends(get_current_user)):
     return {"user": user}
 
 
+# How does read_profile get "user"? Does read_settings get it the same way?
 @app.get("/settings")
 def read_settings():
     return {"user": user, "settings": {}}
