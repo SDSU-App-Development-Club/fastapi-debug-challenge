e8336b242330cdcef2ba38c15dba1b16176871d3
jadengong <ong.jadengabriel@gmail.com>
2026-02-20 17:03:48 -0800

    update bug 6

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# How does FastAPI usually expect you to describe a request body with multiple fields?
+
 @app.post("/items/")
 def create_item(name: str, price: float):
     return {"name": name, "price": price}
