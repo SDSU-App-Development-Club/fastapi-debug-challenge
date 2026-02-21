1d970c2d40c819a46d8878def558cfdc3cc6894f
jadengong <ong.jadengabriel@gmail.com>
2026-02-20 17:00:50 -0800

    update bug 4

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ users_db = {
     2: {"name": "Bob"}
 }
 
+# This code will run, but that's not the bug we're looking for. 
 
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     if user_id not in users_db:
+        # Try requesting /users/999 and check what status code you get. Is it the right one?
         raise HTTPException(status_code=500, detail="User not found")
     return users_db[user_id]
